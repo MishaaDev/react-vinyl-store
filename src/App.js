048513631b46a6f87dc8e2fe3ba26563bc042d05
Mikhail Suvorov <mr.plaim@gmail.com>
2022-08-04 01:20:19 +0300

    complete layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,59 @@
 function App() {
   return (
     <div className="wrapper clear">
+      <div style={{display: "none"}} className="drawer_box">
+        <div className="drawer">
+          <h2>Корзина</h2>
+          <div className="items">
+            <div className="cart_item_box">
+              <div className="cart_item">
+              <img className="item_photo" width={70} height={70} src="/img/items/pink-floyd-wish-you-were-here.jpg" alt="cover" />
+              <div>
+                <div className="item_name">
+                  <p>Pink Floyd</p>
+                  <p>Wish You Were Here</p>
+                </div>
+                <b>2 394 руб.</b>
+              </div>
+              </div>
+              <img className="remove_btn" src="/img/delete1.svg" alt="Remove" />
+            </div>
+            <div className="cart_item_box">
+              <div className="cart_item">
+              <img className="item_photo" width={70} height={70} src="/img/items/pink-floyd-wish-you-were-here.jpg" alt="cover" />
+              <div>
+                <div className="item_name">
+                  <p>Pink Floyd</p>
+                  <p>Wish You Were Here</p>
+                </div>
+                <b>2 394 руб.</b>
+              </div>
+            </div>
+            <img className="remove_btn" src="/img/delete1.svg" alt="Remove" />
+            </div>
+          </div>
+          
+          <ul className="cart_price">
+            <li>
+              <span>Итого:</span>
+              <div></div>
+              <b>21 343 руб.</b>
+            </li>
+            <li>
+              <span>Налог 5%:</span>
+              <div></div>
+              <b>1 200 руб.</b>
+            </li>
+          </ul>
+          <button className="green_button">
+            Оформить заказ
+            <img src="/img/arrow.svg" alt="arrow" />
+          </button>
+        </div>
+      </div>
       <header>
         <div className="header_left">
-          <img width={40} height={40} src="/img/logo.svg" alt="logo" />
+          <img width={50} height={50} src="/img/logo.svg" alt="logo" />
           <div className="header_logo_text">
             <h2>React Vinyl Store</h2>
             <p>Магазин виниловых пластинок</p>
